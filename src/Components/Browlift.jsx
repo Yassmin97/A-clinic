8781fc2dedd341d8b1cdeb9527cd4ca5dc316dda
Yasum <yasmin97a@yahoo.com>
2025-07-05 10:30:43 +0200

    fixed bg img

--- src/Components/Browlift.jsx
+++ src/Components/Browlift.jsx
@@ -4,10 +4,14 @@ import { FaClock } from "react-icons/fa";
 const Browlift = () => {
   return (
     <div>
-    <div className="w-full md:w-352 h-138 bg-right bg-no-repeat" style={{backgroundImage: `url('/Browlift2.jpeg')`}}>
-      <div className="w-full md:w-180 h-138 bg-[center_top_50%] bg-lift bg-no-repeat" style={{backgroundImage: `url('/browlift.jpeg')`}}>
-
-        
+<div
+    className="hidden md:block w-full md:w-352 h-138 bg-right bg-no-repeat absolute top-21 left-0 z-0"
+    style={{ backgroundImage: `url('/Browlift2.jpeg')` }}
+  ></div>
+ <div
+    className="w-full md:w-180 h-138 bg-[center_top_50%] bg-no-repeat relative z-10"
+    style={{ backgroundImage: `url('/browlift.jpeg')` }}
+  >
     <div className="bg-black/35 w-full md:w-352 h-138 px-6 md:px-30 py-34 md:py-40">
       <h1 className="text-4xl md:text-5xl font-bold pt-17 text-white mb-6">Browlift</h1>
       <h4 className="text-2xl md:text-2xl font-bold text-white mb-6">Formade och fylligare bryn</h4>
@@ -16,7 +20,7 @@ const Browlift = () => {
         Boka Nu
         </a>
     </div>
-    </div> </div>
+    </div> 
     
 <div className="max-w-7xl mx-auto px-4 mt-24 mb-30">
     <div className="bg-gray-100 shadow-lg rounded-lg p-6 md:p-8 md:px-10 grid grid-cols-1 md:grid-cols-2 gap-10 items-start">
@@ -34,7 +38,7 @@ const Browlift = () => {
 
     <div className="rounded overflow-hidden md:pl-30 pl-0">
         <img src="/browlift.jpeg" alt="browlift" className="rounded-lg shadow-md md:w-80 w-full h-auto" />
-            <a href="https://www.bokadirekt.se/places/a-clinic-60706" className="inline-block text-white bg-[#4287f5] font-bold text-lg hover:bg-[#c5c5c5] py-3 px-6 rounded transition md:ml-25 ml-28 mt-6 md:mt-10 md:w-auto">
+            <a href="https://www.bokadirekt.se/places/a-clinic-60706" className="inline-block text-white bg-[#4287f5] font-bold text-lg hover:bg-[#c5c5c5] py-3 px-6 rounded transition md:ml-22 ml-28 mt-6 md:mt-10 md:w-auto">
         Boka Nu
         </a>
     </div>
