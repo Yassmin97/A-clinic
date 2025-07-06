af35f57d5e9540e6fe01dc2da947f0cfed25e052
Yasum <yasmin97a@yahoo.com>
2025-07-06 14:56:20 +0200

    updated

--- src/Components/CookieBanner.jsx
+++ src/Components/CookieBanner.jsx
@@ -26,7 +26,7 @@ const CookieBanner = () => {
   return (
     <div className="fixed bottom-0 left-0 w-full z-50 bg-white shadow-lg p-4 md:bottom-4 md:left-4 md:right-4 md:rounded-lg max-w-5xl mx-auto md:mb-4 md:px-6 md:flex md:items-center md:justify-between">
       <p className="text-sm text-gray-800 mb-3 md:mb-0 md:mr-4">
-        Vi använder cookies för att förbättra din upplevelse. Genom att klicka på "Acceptera" godkänner du vår användning av cookies.
+      Genom att klicka på "Acceptera" godkänner du vår användning av cookies.
       </p>
        <div className="flex gap-3">
       <button
